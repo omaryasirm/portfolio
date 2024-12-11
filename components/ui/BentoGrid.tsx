9a788b57dbab9e19767f927373f809768f83a245
Omar Yasir <omaryasirm@gmail.com>
2024-12-11 19:04:58 +0500

    Change GradientBg import ssr to false

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -6,11 +6,14 @@ import Lottie from "react-lottie";
 
 import { cn } from "@/lib/utils";
 
+const BackgroundGradientAnimation = dynamic(() => import("./GradientBg"), {
+  ssr: false,
+});
 
-import { BackgroundGradientAnimation } from "./GradientBg";
 import GridGlobe from "./GridGlobe";
 import animationData from "@/data/confetti.json";
 import MagicButton from "../MagicButton";
+import dynamic from "next/dynamic";
 
 export const BentoGrid = ({
   className,
@@ -99,8 +102,9 @@ export const BentoGridItem = ({
           )}
         </div>
         <div
-          className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"
-            } `}
+          className={`absolute right-0 -bottom-5 ${
+            id === 5 && "w-full opacity-80"
+          } `}
         >
           {spareImg && (
             <img
@@ -176,8 +180,9 @@ export const BentoGridItem = ({
               {/* remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
               {/* add handleCopy() for the copy the text */}
               <div
-                className={`absolute -bottom-5 right-0 ${copied ? "block" : "block"
-                  }`}
+                className={`absolute -bottom-5 right-0 ${
+                  copied ? "block" : "block"
+                }`}
               >
                 {/* <img src="/confetti.gif" alt="confetti" /> */}
                 <Lottie options={defaultOptions} height={200} width={400} />
